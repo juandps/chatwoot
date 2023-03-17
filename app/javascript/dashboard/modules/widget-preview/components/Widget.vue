8d82c2cdbdac4a203a97638bc87a0efc82e6edd1
juandps <juandiegopalacio@aol.com>
2023-03-17 15:46:33 -0400

    bubble logo

--- app/javascript/dashboard/modules/widget-preview/components/Widget.vue
+++ app/javascript/dashboard/modules/widget-preview/components/Widget.vue
@@ -36,7 +36,7 @@
       >
         <img
           v-if="!isWidgetVisible"
-          src="~dashboard/assets/images/bubble-logo.png"
+          src="~dashboard/assets/images/bubble-logo.svg"
           alt=""
         />
         <div>
